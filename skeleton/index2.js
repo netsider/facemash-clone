70c4759c8d2aae494e8ef397d8d8701cd5613706
Russell R <rdoubleoc@aol.com>
2020-09-19 22:13:59 -0400

    Create index2.js

---
+++ skeleton/index2.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+const express  = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+
+//app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(express.json());
+
+const publicDir = "files";
+app.use(express.static(__dirname + "/" + publicDir));
+app.set("view engine", "ejs");
+app.listen(3000);
+
+console.log("Starting...");
+
+app.get("/", function(req, res){
+	fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+});
+
+app.post("/route", function(req, res){
+
+});
