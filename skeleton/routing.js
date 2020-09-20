25e9920495a1952da7ada391f599eba998af23c9
Russell R <rdoubleoc@aol.com>
2020-09-19 23:40:47 -0400

    misc.

--- skeleton/routing.js
+++ skeleton/routing.js
@@ -17,7 +17,7 @@ app.listen(3000);
 
 console.log("Starting...");
 
-app.get("/", function(req, res){
+app.get('/', function(req, res){
 	fs.readFile('index.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
@@ -29,10 +29,43 @@ app.get('/route1', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-app.post("/route2", function(req, res){
+app.post('/route2', function(req, res){
 
 });
 
 app.get('/route3', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 });
+
+
+
+app.get('/flights/:from-:to', function (req, res) { // http://localhost:3000/flights/LAX-SFO
+  console.log(req.params); // { "userId": "34", "bookId": "8989" }
+})
+
+
+// An array of callback functions can handle a route
+var cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+var cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+var cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2])
+
+// More than one callback can handle a route:
+
+app.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, function (req, res) {
+  res.send('Hello from B!')
+})
