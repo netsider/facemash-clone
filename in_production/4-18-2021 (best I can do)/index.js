2e0fa8658fdafc20266b51b270f5d9b4867e0f60
Russell R <rdoubleoc@aol.com>
2021-04-18 03:12:15 -0400

    Update index.js

--- in_production/4-18-2021 (best I can do)/index.js
+++ in_production/4-18-2021 (best I can do)/index.js
@@ -534,6 +534,7 @@ app.post('/loggedin', function(req, res){
 						Promise.all([insertUserIntoDB]).then((values) => { // After promise fulfilled, send object we created earlier.
 							console.log("Result after inserting user into DB: ", values);
 							res.json(obj); // Send object to browser
+							next();
 							// res.redirect('/facemash'); // I knew this wouldn't work
 						});
 						
